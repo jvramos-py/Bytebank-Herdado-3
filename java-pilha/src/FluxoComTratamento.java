903e8c6499d40a6476ce916f263f195f11c94ad8
jvramos-py <jvramos411@gmail.com>
2022-09-16 15:56:14 -0300

    Lançando a primeira exceção

--- java-pilha/src/FluxoComTratamento.java
+++ java-pilha/src/FluxoComTratamento.java
@@ -1,5 +1,4 @@
-public class Fluxo {
-
+public class FluxoComTratamento {
     public static void main(String[] args) {
         System.out.println("Ini do main");
         try {
@@ -20,8 +19,12 @@ public class Fluxo {
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        
-        throw new ArithmeticException("Num deu não caraio kkkkkk");
-        //System.out.println("Fim do metodo2");        
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            System.out.println(i/0);
+            //Conta c = null;
+            //c.deposita();
+        }
+        System.out.println("Fim do metodo2");        
     }
 }
